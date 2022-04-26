f4ef4560a52c3c1895830e8befa51a0fb769b61a
ajing2020 <1277483876@qq.com>
2022-04-26 16:59:19 +0800

    安装devtool 删除json-server相关

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { loadDevTools } from 'jira-dev-tool'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+loadDevTools(() => {
+  const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+  )
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
 reportWebVitals()
