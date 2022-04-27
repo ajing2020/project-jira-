39e245f4c54e0889c018e9142e1ceb9794d87445
ajing2020 <1277483876@qq.com>
2022-04-27 16:02:51 +0800

    auth-provider的实现以及用usecontext全局存储用户信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import LoginScreen from 'screens/login'
-import { ProjectListScreen } from 'screens/project-list'
+// import { ProjectListScreen } from 'screens/project-list'
 
 function App() {
   return (
