bc3e05e7265321e162ac49842da6e40bec1f789d
ajing2020 <1277483876@qq.com>
2022-04-29 14:28:38 +0800

    用Row组件布局页面

--- src/authenticatedApp.tsx
+++ src/authenticatedApp.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
 
@@ -7,10 +8,10 @@ export const AuthenticatedApp = () => {
   return (
     <Container>
       <Header>
-        <HeaderLeft>
-          <h3>logo</h3>
-          <h3>项目</h3>
-          <h3>用户</h3>
+        <HeaderLeft gap={true}>
+          <h2>logo</h2>
+          <h2>项目</h2>
+          <h2>用户</h2>
         </HeaderLeft>
         <HeaderRight>
           <button onClick={logout}>登出</button>
@@ -25,22 +26,15 @@ export const AuthenticatedApp = () => {
 
 const Container = styled.div`
   display: grid;
-  grid-template-rows: 6rm 1fr;
+  grid-template-rows: 6rem 1fr;
   height: 100vh;
 `
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+// grid-area 用来给grid子元素起名字
+const Header = styled(Row)`
+  padding: 3.2rem;
+  box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.1);
+  z-index: 1;
 `
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-`
-const HeaderRight = styled.div``
-const Main = styled.main`
-  grid-area: main;
-`
+const HeaderLeft = styled(Row)``
+const HeaderRight = styled.header``
+const Main = styled.main``
