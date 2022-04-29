9dc587677d220b77d154f9baca77b207a102bc46
ajing2020 <1277483876@qq.com>
2022-04-29 15:45:17 +0800

    部分遗留bug修复

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { loadDevTools } from 'jira-dev-tool'
+import { loadServer, DevTools } from 'jira-dev-tool'
 //务必在jira-dev-tool后引入
 import 'antd/dist/antd.less'
 import { AppProviders } from 'context'
 
-loadDevTools(() => {
+loadServer(() => {
   const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
   )
   root.render(
     <React.StrictMode>
       <AppProviders>
+        <DevTools />
         <App />
       </AppProviders>
     </React.StrictMode>
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,16 +1,16 @@
 import { useEffect, useState } from 'react'
 
 export const isFalsy = (value: unknown) => (value === 0 ? false : !value)
+export const isVoid = (value: unknown) =>
+  value === undefined || value === null || value === ''
 
 //在函数中改变传入的对象是不好的
-interface resObj {
-  [key: string]: any
-}
-export const cleanObject = (object: object) => {
-  const result: resObj = { ...object }
+
+export const cleanObject = (object: { [key: string]: unknown }) => {
+  const result = { ...object }
   Object.keys(result).forEach((key: string) => {
     const value = result[key]
-    if (isFalsy(value)) {
+    if (isVoid(value)) {
       delete result[key]
     }
   })
