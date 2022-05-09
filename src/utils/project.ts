2a49b27d9550ecd9ccdf5ce5215a9480e29488ee
ajing2020 <1277483876@qq.com>
2022-05-09 14:59:36 +0800

    阻止在已卸载组件上赋值

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -14,7 +14,7 @@ export const useProjects = (param?: Partial<Project>) => {
 
   useEffect(() => {
     run(fetchProjects(), {
-      retry: fetchProjects
+      retry: fetchProjects 
     })
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [param])
